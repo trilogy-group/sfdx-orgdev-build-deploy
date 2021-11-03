90adae7ab84c3c8c359fc373974e9daae632e3b0
Mohamed El Hagry <mohamed.el.hagry@trilogy.com>
2021-11-03 22:17:19 +0200

    LAMBDA-19114: Nightly sync - include audit log as commit comment modify output file to write only returned data and overwrite  the file

--- src/utils/exec-command.js
+++ src/utils/exec-command.js
@@ -21,7 +21,7 @@ const getErrorMessage = function(spawn) {
 
 const outputMessage = function(message, type, outputStdout) {
     let fileName = './output.txt';
-    let fileFlags = {flag: 'as'};
+    let fileFlags = {flag: 'w'};
 
     if (type == 'error') {
         core.error(message);
@@ -58,9 +58,11 @@ module.exports.run = function(command, args, workingFolder = null, process = nul
     }
 
     if (spawn.stdout) {
-        outputMessage("Command executed: " + command, 'info', outputStdout);
-        outputMessage("With the following args: " + args.toString(), 'info', outputStdout);
-        outputMessage("Having the following return: " + spawn.stdout.toString(), 'stdout', outputStdout);
+
+        //commented not needed in the output as the output needed to be stored is the returned data  LAMBDA-19114
+        //outputMessage("Command executed: " + command, 'info', outputStdout);
+        //outputMessage("With the following args: " + args.toString(), 'info', outputStdout);
+        //outputMessage("Having the following return: " + spawn.stdout.toString(), 'stdout', outputStdout);
 
         switch (process) {
             case 'authInSandbox':
